328f57271e42ef4ae15dcce3d9ec62c3a64ace3c
Hazem <h_comender@hotmail.com>
2025-03-04 21:00:11 +0200

    initial commit with basic UI

--- app/src/main/java/com/example/mynote/model/Note.kt
+++ app/src/main/java/com/example/mynote/model/Note.kt
@@ -1,9 +1,11 @@
 package com.example.mynote.model
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.time.LocalDateTime
 import java.util.UUID
 
-data class Note(
+data class Note @RequiresApi(Build.VERSION_CODES.O) constructor(
     val id: UUID = UUID.randomUUID(),  // will create random, unique universal ids linked to each note
     val title: String,
     val description: String,
